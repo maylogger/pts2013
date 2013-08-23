50f5245094bcc429bdfa7d769e65c87e2224e7b3
shengli1989 <shengli@evendesign.tw>
2013-08-23 18:22:32 +0800

    off canvas shows

--- javascripts/all.js
+++ javascripts/all.js
@@ -2,3 +2,17 @@ $(".menu-btn").click(function(){
   $(".container").toggleClass("show-menu");
   return false;
 });
+
+
+
+
+$(".shows-btn").click(function(){
+  $(".container").toggleClass("show-shows");
+  return false;
+});
+
+
+// 點擊任何一處，關閉已開啟的 .show-collapse
+// $(document).on('click',function(){
+//   $('.shows-collapse').collapse('hide');
+// })
