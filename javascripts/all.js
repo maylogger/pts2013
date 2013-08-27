2fe42a42920bf413166decacbd9c68343825390c
shengli1989 <shengli@evendesign.tw>
2013-08-27 18:34:20 +0800

    刪除多餘程式、修補 collapse 缺失

--- javascripts/all.js
+++ javascripts/all.js
@@ -3,16 +3,7 @@ $(".menu-btn").click(function(){
   return false;
 });
 
-
-
-
 $(".shows-btn").click(function(){
   $(".container").toggleClass("show-shows");
   return false;
 });
-
-
-// 點擊任何一處，關閉已開啟的 .show-collapse
-// $(document).on('click',function(){
-//   $('.shows-collapse').collapse('hide');
-// })
