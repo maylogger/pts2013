f9caf14ca5ebf72549f6f9aadc4cddce08290655
shengli1989 <shengli@evendesign.tw>
2013-11-11 15:07:19 +0800

    fixed click off-cavas-menu bug

--- javascripts/all.js
+++ javascripts/all.js
@@ -3,7 +3,10 @@ $(".menu-btn").click(function(){
   $(".container").toggleClass("show-menu");
   return false;
 });
-
+$(".off-canvas-menu").click(function(){
+  event.stopPropagation();
+  return false;
+});
 $(".shows-btn").click(function(){
   event.stopPropagation();
   $(".container").toggleClass("show-shows");
@@ -37,4 +40,37 @@ $(document).click(function(){
 $('body').imagesLoaded().done( function( instance ) {
   console.log('all images successfully loaded');
   $('body').removeClass('img-loading');
+});
+
+
+// test
+$('.video').magnificPopup({
+  type: 'iframe',
+
+
+  iframe: {
+    patterns: {
+      dailymotion: {
+
+        index: 'dailymotion.com',
+
+        id: function(url) {
+            var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
+            if (m !== null) {
+                if(m[4] !== undefined) {
+
+                    return m[4];
+                }
+                return m[2];
+            }
+            return null;
+        },
+
+        src: 'http://www.dailymotion.com/embed/video/%id%'
+
+      }
+    }
+  }
+
+
 });
