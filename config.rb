74c6b66555d5d0d47ddd0da9bb5ee571cbae6d31
evenwu <evendesign@gmail.com>
2014-01-07 16:53:20 +0800

    大幅移動文件與pts伺服器相符

--- config.rb
+++ config.rb
@@ -21,13 +21,13 @@ asset_cache_buster do |http_path, real_path|
  nil
 end
 
-css_dir = "stylesheets" # by Fire.app
+css_dir = "homepage/stylesheets" # by Fire.app
 sass_dir = "sass" # by Fire.app
-images_dir = "images" # by Fire.app
-javascripts_dir = "javascripts" # by Fire.app
+images_dir = "homepage/images" # by Fire.app
+javascripts_dir = "homepage/javascripts" # by Fire.app
 fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app
 fireapp_livescripts_dir = "livescripts" # by Fire.app
-fireapp_minifyjs_on_build = false # by Fire.app
+fireapp_minifyjs_on_build = true # by Fire.app
 fireapp_always_report_on_build = false # by Fire.app
 relative_assets = false # by Fire.app
 line_comments = false # by Fire.app
@@ -35,4 +35,4 @@ sass_options = {:debug_info=>false} # by Fire.app
 fireapp_coffeescript_options = {:bare=>false} # by Fire.app
 fireapp_livescript_options = {:bare=>false} # by Fire.app
 
-output_style = :expanded # by Fire.app
+output_style = :compressed # by Fire.app
